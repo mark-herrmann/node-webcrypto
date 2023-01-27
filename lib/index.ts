e4b3c4bdc1c611cf5cd14b773bb61f94a6dbe21c
Mark Herrmann <mark.herrmann@mailbox.org>
2023-01-27 13:18:19 +0100

    improvements

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,5 @@
 import nodeCrypto from 'crypto';
 
-// Installing and importing node-forge provides type definitions for webcrypto
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import nodeForge from 'node-forge';
-
 const { webcrypto } = nodeCrypto;
 
 export default webcrypto;
